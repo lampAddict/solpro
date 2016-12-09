15209f580f92e54938b0837ee58834f6a93fb493
Roman Makarov <rk_box@mail.ru>
2016-12-09 11:26:01 +0300

    mock pages refactoring

---
+++ web/static/js/main.js
@@ -0,0 +1,25 @@
+function showMenuItems() {
+    var x = document.getElementById("menu");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$( document ).ready(function(){
+
+    $('#filter').click(function(e){
+
+        var _filterBlock = $('#filterBlock');
+        if( _filterBlock.is(':visible') ){
+            _filterBlock.slideUp();
+            $(e.currentTarget).text('Показать фильтры');
+        }
+        else{
+            _filterBlock.slideDown();
+            $(e.currentTarget).text('Убрать фильтры');
+        }
+        return false;
+    });
+});
