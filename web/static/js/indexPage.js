8067471e4b0385568e10d82d7d80a9880ba46ffe
Roman Makarov <rk_box@mail.ru>
2016-12-09 20:56:30 +0300

    centered mock pages

--- web/static/js/indexPage.js
+++
@@ -1,8 +0,0 @@
-function showMenuItems() {
-    var x = document.getElementById("menu");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
