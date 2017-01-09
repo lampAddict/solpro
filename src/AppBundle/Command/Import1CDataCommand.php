68de26236f7c9f6f81c90d08551a993927915cbf
Roman Makarov <rk_box@mail.ru>
2017-01-09 15:51:02 +0300

    import 1c data, parsing data routine

--- src/AppBundle/Command/Import1CDataCommand.php
+++ src/AppBundle/Command/Import1CDataCommand.php
@@ -10,6 +10,9 @@ use Symfony\Component\DomCrawler\Crawler;
 class Import1CDataCommand extends Command
 {
     protected $refs;
+    protected $routes;
+    protected $orders;
+    protected $lots;
 
     protected function configure()
     {
@@ -22,7 +25,7 @@ class Import1CDataCommand extends Command
 
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp("This command allows you to get up to date data from 1C")
+            ->setHelp("This command allows you to import routes data onto portal from 1C")
         ;
     }
 
@@ -31,8 +34,8 @@ class Import1CDataCommand extends Command
         $output->writeln('Import data started..');
 
         $data = new Crawler(file_get_contents('web/data.xml'));
-
         if( $data ){
+            //Parsing references block
             $refs = $data->filter('MessageFrom1C > references')->children();
             if( !empty($refs) ){
                 foreach( $refs as $ref ){
@@ -60,10 +63,96 @@ class Import1CDataCommand extends Command
                         }
                     }
                 }
-
-                var_dump($this->refs);
-                die;
             }
+
+            //Parsing routes block
+            $routes = $data->filter('MessageFrom1C > routes')->children();
+            if( !empty($routes) ){
+                $count = 1;
+                foreach ($routes as $route){
+                    $routeData = $data->filter('MessageFrom1C > routes > route:nth-child('.$count.')')->children();
+
+                    $el = [];
+                    $key = '';
+                    foreach ($routeData as $routeDataItem){
+
+                        $nodeValue = $routeDataItem->nodeValue;
+
+                        if( $routeDataItem->nodeName == 'id' ){
+                            $key = $routeDataItem->nodeValue;
+                        }
+
+                        //Parsing orders block
+                        if( $routeDataItem->nodeName == 'orders' ){
+
+                            $orders = $data->filter('MessageFrom1C > routes > route:nth-child('.$count.') > orders')->children();
+                            if( !empty($orders) ){
+                                $nodeValue = [];
+                                $count1 = 1;
+                                foreach ($orders as $order){
+                                    $ordersData = $data->filter('MessageFrom1C > routes > route:nth-child('.$count.') > orders > order:nth-child('.$count1.')')->children();
+                                    $elOrder = [];
+                                    $keyOrder = '';
+                                    foreach ($ordersData as $ordersDataItem){
+                                        if( $ordersDataItem->nodeName == 'id' ){
+                                            $keyOrder = $ordersDataItem->nodeValue;
+                                        }
+
+                                        $elOrder[ $ordersDataItem->nodeName ] = $ordersDataItem->nodeValue;
+                                    }
+
+                                    if(    $keyOrder != ''
+                                        && !empty($elOrder)
+                                    ){
+                                        $this->orders[ $keyOrder ] = $elOrder;
+                                    }
+
+                                    $count1++;
+                                    $nodeValue[] = $elOrder;
+                                }
+                            }
+                        }
+
+                        $el[ $routeDataItem->nodeName ] = $nodeValue;
+                    }
+
+                    if(    $key != ''
+                        && !empty($el)
+                    ){
+                        $this->routes[ $key ] = $el;
+                    }
+
+                    $count++;
+                }
+            }
+
+            //Parsing lots block
+            $lots = $data->filter('MessageFrom1C > lots')->children();
+            if( !empty($lots) ){
+                $count = 1;
+                foreach ($lots as $lot){
+                    $lotData = $data->filter('MessageFrom1C > lots > lot:nth-child('.$count.')')->children();
+
+                    $el = [];
+                    $key = '';
+                    foreach ($lotData as $lotDataItem){
+                        if( $lotDataItem->nodeName == 'id' ){
+                            $key = $lotDataItem->nodeValue;
+                        }
+
+                        $el[ $lotDataItem->nodeName ] = $lotDataItem->nodeValue;;
+                    }
+
+                    if(    $key != ''
+                        && !empty($el)
+                    ){
+                        $this->lots[ $key ] = $el;
+                    }
+
+                    $count++;
+                }
+            }
+
         }
     }
 }
