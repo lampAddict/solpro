e960b2491b9fb49ca891635a13b8a248adfb4293
Roman Makarov <rk_box@mail.ru>
2017-01-26 17:21:58 +0300

    auction page load failed fix

--- src/AppBundle/Controller/AuctionController.php
+++ src/AppBundle/Controller/AuctionController.php
@@ -47,25 +47,26 @@ class AuctionController extends Controller
             ->getResult();
 
         $forms = [];
-        foreach( $lots as $lot ){
-            $bet = new Bet();
-            
-            $bet->setLotId($lot->getId());
-            
-            $form = $this->createForm('AppBundle\Form\BetType', $bet, ['lot'=>$lot]);
+        if( !empty($lots) ){
+            foreach( $lots as $lot ){
+                $bet = new Bet();
 
-            $form->handleRequest($request);
-            if(    $form->isSubmitted()
-                && $form->isValid()
-                && intval($request->request->get('appbundle_bet')['lot_id']) == $lot->getId()
-            ){
-                //check if user authenticated
-                if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
-                    throw $this->createAccessDeniedException();
-                }
-                //$user = $this->get('security.token_storage')->getToken()->getUser();
+                $bet->setLotId($lot->getId());
 
-                $lot = $em
+                $form = $this->createForm('AppBundle\Form\BetType', $bet, ['lot'=>$lot]);
+
+                $form->handleRequest($request);
+                if(    $form->isSubmitted()
+                    && $form->isValid()
+                    && intval($request->request->get('appbundle_bet')['lot_id']) == $lot->getId()
+                ){
+                    //check if user authenticated
+                    if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+                        throw $this->createAccessDeniedException();
+                    }
+                    //$user = $this->get('security.token_storage')->getToken()->getUser();
+
+                    $lot = $em
                         ->getRepository('AppBundle:Lot')
                         ->createQueryBuilder('l')
                         ->leftJoin('l.routeId', 'r')
@@ -73,33 +74,37 @@ class AuctionController extends Controller
                         ->setMaxResults( 1 )
                         ->getQuery()
                         ->getResult();
-                
-                $bet = new Bet();
-                
-                /* @var $lot \AppBundle\Entity\Lot */
-                $lot = $lot[0];
 
-                $bet->setLotId( $lot->getId() );
-                $bet->setUserId( $this->getUser() );
-                $bet->setCreatedAt(new \DateTime());
-                if(    intval($request->request->get('appbundle_bet')['value']) <= $lot->getPrice() - $lot->getRouteId()->getTradeStep()
-                    && intval($request->request->get('appbundle_bet')['value']) > 0
-                    && ($lot->getStartDate()->getTimestamp() + $lot->getDuration()) < time()
-                ){
-                    $bet->setValue( intval($request->request->get('appbundle_bet')['value']) );
-                    $lot->setPrice( intval($request->request->get('appbundle_bet')['value']) );
+                    $bet = new Bet();
 
-                    $em->persist($bet);
-                    $em->persist($lot);
+                    /* @var $lot \AppBundle\Entity\Lot */
+                    $lot = $lot[0];
 
-                    $em->flush();
-                    
-                    $redis->set('lcp_'.$lot->getId(), json_encode(['price'=>$lot->getPrice(), 'owner'=>$this->getUser()->getId()]));
-                    //$this->get('memcache.default')->set('lcp_'.$lot->getId(), $lot->getPrice(), 0, 1*60*60);
+                    $bet->setLotId( $lot->getId() );
+                    $bet->setUserId( $this->getUser() );
+                    $bet->setCreatedAt(new \DateTime());
+                    if(    intval($request->request->get('appbundle_bet')['value']) <= $lot->getPrice() - $lot->getRouteId()->getTradeStep()
+                        && intval($request->request->get('appbundle_bet')['value']) > 0
+                        && ($lot->getStartDate()->getTimestamp() + $lot->getDuration()) < time()
+                    ){
+                        $bet->setValue( intval($request->request->get('appbundle_bet')['value']) );
+                        $lot->setPrice( intval($request->request->get('appbundle_bet')['value']) );
+
+                        $em->persist($bet);
+                        $em->persist($lot);
+
+                        $em->flush();
+
+                        $redis->set('lcp_'.$lot->getId(), json_encode(['price'=>$lot->getPrice(), 'owner'=>$this->getUser()->getId()]));
+                        //$this->get('memcache.default')->set('lcp_'.$lot->getId(), $lot->getPrice(), 0, 1*60*60);
+                    }
                 }
-            }
 
-            $forms[ $lot->getId() ] = $form->createView();
+                $forms[ $lot->getId() ] = $form->createView();
+            }
+        }
+        else{
+            $lots = [];
         }
         
         return $this->render('auctionPage.html.twig', array(
