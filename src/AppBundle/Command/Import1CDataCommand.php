e716b975df4448f5c1b3773d2241f4bfd949d93c
Roman Makarov <rk_box@mail.ru>
2017-02-10 14:58:21 +0300

    import lots data from 1c additional parameter removed

--- src/AppBundle/Command/Import1CDataCommand.php
+++ src/AppBundle/Command/Import1CDataCommand.php
@@ -28,20 +28,20 @@ class Import1CDataCommand extends ContainerAwareCommand
             // the full command description shown when running the command with
             // the "--help" option
             ->setHelp("This command allows you to import auction lots data onto portal from 1C")
-            ->addArgument('downloadFromFTP', InputArgument::OPTIONAL, 'Download data from ftp ?')
+            //->addArgument('downloadFromFTP', InputArgument::OPTIONAL, 'Download data from ftp ?')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $downloadFromFTP = $input->getArgument('downloadFromFTP');
-        if( $downloadFromFTP ){
+        //$downloadFromFTP = $input->getArgument('downloadFromFTP');
+        //if( $downloadFromFTP ){
             $output->writeln('Connecting to ftp..');
             $conn_id = ftp_connect('10.32.2.19') or die("Couldn't establish connection to ftp server");//ftp.solpro.ru
             if( !ftp_login($conn_id, 'ftp_1c', 'cURz46mGDs') )die("Couldn't login to ftp server");
             if( ftp_get($conn_id, 'data/data.xml', 'MessageFrom1C.xml', FTP_BINARY) )$output->writeln('XML file downloaded successfully');
             ftp_close($conn_id);
-        }
+        //}
 
         $output->writeln('Import data started..');
 
