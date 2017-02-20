0eaa75435b39e931591cb54d0e9a145b4790dc19
Roman Makarov <rk_box@mail.ru>
2017-02-20 18:16:12 +0300

    auction place bet js, scroll to the same lot after page reload 5

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -251,7 +251,7 @@ $( document ).ready(function(){
                         scrollTop: $this.offset().top
                     }, 500);
 
-                    document.location.href.replace(/#lot-[\d]+/g, '');
+                    document.location.href = document.location.href.replace(/#lot-[\d]+/g, '');
                     document.location.href = document.location.href + '#lot-' + _lotId;
                 });
                 //call submit routine
