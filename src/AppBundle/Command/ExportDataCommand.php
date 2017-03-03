bcade9221f1e6ce50640f3b86b1f3d91e4f3740a
Roman Makarov <rk_box@mail.ru>
2017-03-03 17:15:54 +0300

    data exchange routine, export data file creation debug 6

--- src/AppBundle/Command/ExportDataCommand.php
+++ src/AppBundle/Command/ExportDataCommand.php
@@ -5,7 +5,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ExportDataCommand extends Command
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+class ExportDataCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
