b9bac02dd1dacfe3b3299540616e722e89429a30
Roman Makarov <rk_box@mail.ru>
2017-03-06 16:13:19 +0300

    new reference entityfor passport data

---
+++ src/AppBundle/Entity/RefPassport.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="refPassport")
+ */
+class RefPassport
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    protected $id1C;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set id1C
+     *
+     * @param string $id1C
+     *
+     * @return RefPassport
+     */
+    public function setId1C($id1C)
+    {
+        $this->id1C = $id1C;
+
+        return $this;
+    }
+
+    /**
+     * Get id1C
+     *
+     * @return string
+     */
+    public function getId1C()
+    {
+        return $this->id1C;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return RefPassport
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
