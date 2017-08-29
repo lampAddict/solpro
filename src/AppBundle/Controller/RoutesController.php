5b430fbf5a9e754a11e5bef02c15f2a6f562c7db
Roman Makarov <rk_box@mail.ru>
2017-08-29 16:06:47 +0300

    routes page, get corresponding routes by carrier

--- src/AppBundle/Controller/RoutesController.php
+++ src/AppBundle/Controller/RoutesController.php
@@ -16,7 +16,7 @@ class RoutesController extends Controller
      * @return string $where composed condition
      */
     private function makeFilterCondition( $_filters ){
-        $where = 'r.user_id = '.$this->getUser()->getId();
+        $where = 'r.carrier = '.$this->getUser()->getCarrierId1C();
 
         $filters = (array) $_filters;
         if( !empty($filters) ){
