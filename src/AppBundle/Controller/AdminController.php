94c5f3b66b628364a53bde6e30e1995bef6d5e93
Roman Makarov <rk_box@mail.ru>
2017-08-30 20:59:36 +0300

    admin page, remove unnecessary query, combine all in one db request, page data changed, lot status added, 0:00 time removed

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -31,7 +31,7 @@ class AdminController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         //get lots data
-        $sql = 'SELECT l.*, u.username as user_name FROM lot l LEFT JOIN bet b ON b.lot_id = l.id LEFT JOIN fos_user u ON b.user_id = u.id WHERE l.price >= IFNULL(b.value, 0) GROUP BY l.id ORDER BY l.start_date DESC, l.auction_status DESC';
+        $sql = 'SELECT l.*, u.username as user_name, r.code as code, r.region_from, r.region_to FROM lot l LEFT JOIN bet b ON b.lot_id = l.id LEFT JOIN fos_user u ON b.user_id = u.id LEFT JOIN route r ON r.lot_id = l.id WHERE l.price >= IFNULL(b.value, 0) GROUP BY l.id ORDER BY l.start_date DESC, l.auction_status DESC';
         $stmt = $em->getConnection()->prepare($sql);
         $stmt->execute();
         $_lots = $stmt->fetchAll();
@@ -41,30 +41,14 @@ class AdminController extends Controller
         if( !empty($_lots) ){
             foreach( $_lots as $indx=>$lot ){
                 $_lots[ $indx ]['start_date']  = new \DateTime( $lot['start_date'] );
-                $routesIds[] = $lot['id'];
             }
         }
         else{
             $_lots = [];
         }
 
-        $_routes = [];
-
-        //get routes data
-        if( !empty($routesIds) ){
-            $sql = 'SELECT * FROM route WHERE lot_id IN ('.join(',', $routesIds).')';
-            $stmt = $em->getConnection()->prepare($sql);
-            $stmt->execute();
-            $routesData = $stmt->fetchAll();
-            foreach( $routesData as $routeData ){
-                $routeData['load_date'] = new \DateTime( $routeData['load_date'] );
-                $_routes[ $routeData['lot_id'] ] = $routeData;
-            }
-        }
-
         return $this->render('adminPage.html.twig', array(
              'lots' => $_lots
-            ,'routes' => $_routes
             ,'tz' => ($this->getUser()->getTimezone() != '' ? $this->getUser()->getTimezone() : 'UTC')
         ));
     }
