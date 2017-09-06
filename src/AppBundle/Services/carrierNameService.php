f882ed120a523576251308c4a509494c651cc758
Roman Makarov <rk_box@mail.ru>
2017-09-06 17:40:36 +0300

    show carrier name near logged in user fix

--- src/AppBundle/Services/carrierNameService.php
+++ src/AppBundle/Services/carrierNameService.php
@@ -6,28 +6,40 @@ class carrierNameService
 {
     protected $em;
     protected $ts;
-    protected $name;
 
     public function __construct($entityManager, $tokenStorage)
     {
-        $this->em    = $entityManager;
-        $this->ts    = $tokenStorage;
-
-        $this->name  = $this->carrierNameService();
+        $this->em = $entityManager;
+        $this->ts = $tokenStorage;
     }
 
     public function carrierNameService(){
-        //get carrier name of current user
-        $sql = 'SELECT rc.name FROM refcarrier as rc LEFT JOIN fos_user u ON u.carrier_id1c = rc.id1c WHERE u.id = "'.$this->ts->getToken()->getUser()->getId().'"';
-        $stmt = $this->em->getConnection()->prepare($sql);
-        $stmt->execute();
-        $carrierName = $stmt->fetchAll();
+    }
 
-        return (is_array($carrierName)?$carrierName[0]['name']:'');
+    public function getCarrierName(){
+        $userId = null;
+        if( $this->ts ){
+            $user =  $this->ts->getToken()->getUser();
+            if( $user ){
+                $userId = $user->getId();
+            }
+        }
+
+        if( !is_null($userId) ){
+            //get carrier name of current user
+            $sql = "SELECT rc.name FROM refcarrier as rc LEFT JOIN fos_user u ON u.carrier_id1c = rc.id1c WHERE u.id = '$userId'";
+            $stmt = $this->em->getConnection()->prepare($sql);
+            $stmt->execute();
+            $carrierName = $stmt->fetchAll();
+
+            return (is_array($carrierName) ? $carrierName[0]['name'] : '');
+        }
+
+        return '';
     }
 
     public function __toString()
     {
-        return $this->name;
+        return $this->getCarrierName();
     }
 }
